9b9eefa9c381fe17db42e4017477be4c8e9217bb
Shivam21512 <ugaleshivam73@gmail.com>
2025-09-15 11:30:07 +0530

    final

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 // Always enforce `/api` at the end of BASE_URL
-const BASE_URL =
-  (process.env.REACT_APP_API_URL || 'http://localhost:5000').replace(/\/$/, '') + '/api';
+const BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
 
 const api = axios.create({
   baseURL: BASE_URL,
